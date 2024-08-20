582c57d1c347469597de74d95a48d3f4107924f2
Bagus Muhammad Wijaksono <baguswijaksono292@gmail.com>
2024-08-20 14:00:58 +0700

    feat: self hosted popper.js & bootstrap.js

--- resources/views/factory/show.blade.php
+++ resources/views/factory/show.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/show.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div class="container">
         <div class="card">
@@ -26,7 +28,7 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-7+/S3MHvZjEUF0MRRqkKRRxNuN8Pbq1XuFzAYvcN/fXtPyhrhjjdOvuZ2M9dPf/e" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRac7mFf2R6VJ9Ht5V4UpEVg9KNV5vhFeEJvF0/jz" crossorigin="anonymous"></script>
-</body>
-</html>
+    <script src="{{ asset('js/popper.min.js') }}">
+        <script src="{{ asset('js/bootstrap.min.js') }}">
+        </body>
+</html >
